81a055f4c1c2d1cc6edae5c496061e4ba26bd514
Leonhard Hofer <117044404+gitb8@users.noreply.github.com>
2023-03-12 13:39:36 +0100

    Add files via upload

---
+++ microbench.nim
@@ -0,0 +1,68 @@
+import times
+
+# set run repetitions:
+const RUNS: int = 65536
+
+var start: Time = getTime()
+var ende: Time = getTime()
+var first: float32
+var second: float32
+
+# add variables for bench:
+
+var a = 0
+
+
+# add functoins for bench:
+
+proc func1(): void = discard        # demo function
+
+proc func2(): void = discard        # demo function
+
+
+start = getTime()
+for run in countup(1, RUNS):
+    # 1st run
+
+    a += 1
+    # a += 2        # reverse test
+
+    # bench functions
+    func1()
+    # func2()       # reverse test
+ende = getTime()
+first = (ende - start).inNanoseconds.float32
+
+# reset bench variables:
+
+a = 0
+
+
+start = getTime()
+for run in countup(1, RUNS):
+    # 2nd run
+
+    a += 2
+    # a += 1        # reverse test
+
+    # bench functions
+    func2()
+    # func1()       # reverse test
+ende = getTime()
+second = (ende - start).inNanoseconds.float32
+
+# print result nanoseconds
+# echo "1st run: ", first / RUNS.float32, " ns"
+# echo "2nd run: ", second / RUNS.float32, " ns"
+
+# print result microseconds
+echo "1st run: ", first / RUNS.float32 / 1000.0, " µs"
+echo "2nd run: ", second / RUNS.float32 / 1000.0, " µs"
+
+# print result milliseconds
+# echo "1st run: ", first / RUNS.float32 / 1.0e6, " ms"
+# echo "2nd run: ", second / RUNS.float32 / 1.0e6, " ms"
+
+# print result seconds
+# echo "1st run: ", first / RUNS.float32 / 1.0e9, " s"
+# echo "2nd run: ", second / RUNS.float32 / 1.0e9, " s"
